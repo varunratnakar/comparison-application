ce9238ec1ea3231c4d1bc73e7574cb2c603b487d
Alex Baker <bakeraj@usc.edu>
2021-03-22 14:07:53 -0700

    testing backend connectin

--- middleware/api.py
+++ middleware/api.py
@@ -52,7 +52,8 @@ def apply_sorting_criteria(trial_data):
 # Sort Trials By Criteria Route
 @app.route('/api/sortTrialsByCriteria', methods=['PUT'])
 def api_sortTrialsByCriteria():
-    print("in sorting");
+    print("in sorting")
+    return "hello"
     keyword = request.form['keyword']
     num_results = request.form['numResults']
 
