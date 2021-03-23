f3992f21fcdc8191aea53f0cbafdf3610c39d30b
Alex Baker <bakeraj@usc.edu>
2021-03-22 18:50:39 -0700

    update

--- middleware/api.py
+++ middleware/api.py
@@ -54,7 +54,8 @@ def apply_sorting_criteria(trial_data):
 # Sort Trials By Criteria Route
 @app.route('/api/sortTrialsByCriteria', methods=['PUT'])
 def api_sortTrialsByCriteria():
-    return "hello"
+    response = Response(status=200)
+    return response
     keyword = request.form['keyword']
     num_results = request.form['numResults']
 
