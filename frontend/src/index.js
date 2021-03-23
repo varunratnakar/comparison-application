8f0bfa8fe52bc974cf8f5010533a168f1cf56d3c
Alex Baker <bakeraj@usc.edu>
2021-03-23 14:29:23 -0700

    displaying result

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -138,7 +138,7 @@ class PatientDisplay extends React.Component {
   */
 
   render(){
-    const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem", margin:"5px"};
+    const BarStyling = {width:"17rem",background:"#F2F1F9", border:"none", padding:"0.5rem", margin:"5px"};
     const WeightStyling = {width:"2rem",background:"#F2F1F9", border:"none", padding:"0.5rem", margin:"5px"}
     return(
       <div className='PatientDisplay'>
