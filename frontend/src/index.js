898e7f0fb21a033fa37a39163cbde59de07b294c
Alex Baker <bakeraj@usc.edu>
2021-03-24 12:28:56 -0700

    testing table

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -288,11 +288,11 @@ class TableDisplay extends React.Component {
     const classes = useStyles;
 
     const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    this.createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    this.createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    this.createData('Eclair', 262, 16.0, 24, 6.0),
+    this.createData('Cupcake', 305, 3.7, 67, 4.3),
+    this.createData('Gingerbread', 356, 16.0, 49, 3.9),
     ];
 
     return (
