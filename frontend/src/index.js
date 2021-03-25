ddf574d1eabba7d8416ef0e103007092ed1aa134
Alex Baker <bakeraj@usc.edu>
2021-03-24 20:07:08 -0700

    testing table

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -340,7 +340,7 @@ class TableDisplay extends React.Component {
                 </TableCell>
                 <TableCell>{row.name}</TableCell>
                 <TableCell align="right" width="200px">{!row.age ? "No match" : "Match"}</TableCell>
-                <TableCell align="right">{!row.condition ? "No match" : "Match"}</TableCell>
+                <TableCell align="right" width="200px">{!row.condition ? "No match" : "Match"}</TableCell>
                 <TableCell align="right">{!row.inclusion ? "No match" : "Match"}</TableCell>
                 <TableCell align="right">{!row.exclusion ? "No match" : "Match"}</TableCell>
                 <TableCell align="right">{!row.completed ? "No match" : "Match"}</TableCell>
